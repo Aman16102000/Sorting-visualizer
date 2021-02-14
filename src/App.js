d6ba0dffe67f0cff7eb523c5d90bdababced4bf4
Aman Thakur <amanthakur5908@gmail.com>
2021-02-14 15:27:14 +0530

    Generated random numbers with visualization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import {Progress} from 'reactstrap';
+class App extends React.Component{
+  state={
+    arr:[]
+  }
+  setNumbers=()=>{
+    var random=[];
+    for (let index = 0; index < 10; index++) {
+      const value=Math.floor(Math.random()*100)+1;
+      
+      random.push(value);
+    }
+    this.setState({
+      arr:random
+    })
+  }
+  render()
+  {
+    
+    //console.log(this.state.arr);
+    return(
+      <div className="row">
+        <div className="col-12 col-sm-12">
+          <div className="row">
+            <div className="col-4 col-sm-4">
+          <button className="btn btn-primary" onClick={this.setNumbers}>Generate numbers</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-sm-12">
+              <br/>
+              {
+                this.state.arr.map((val,index)=>{
+                  return(
+                    <div className="row" key={index}>
+                      <div className="col-8 col-sm-8">
+                        <Progress value={val} />
+                      </div>
+                      <div className="col-4 col-sm-4">{val}</div>
+                      </div>        
+                  );
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
